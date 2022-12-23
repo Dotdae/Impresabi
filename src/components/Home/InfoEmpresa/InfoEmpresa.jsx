6297202ded3ee946d4a07eb9d37edeac23e5fccf
Asvinrol <ramonlerma24@gmail.com>
2022-12-23 09:42:20 -0700

    Vision & Mision

--- src/components/Home/InfoEmpresa/InfoEmpresa.jsx
+++ src/components/Home/InfoEmpresa/InfoEmpresa.jsx
@@ -3,16 +3,16 @@ import { SlNote } from "react-icons/sl";
 import { RiPenNibLine } from "react-icons/ri";
 import { BsImages } from "react-icons/bs";
 import { IoIosRocket} from "react-icons/io";
-import {IoEyeOutline } from "react-icons/io5";
+import { GiArrowScope } from "react-icons/gi";
+import { IoEyeOutline } from "react-icons/io5";
 
 const Quienes = styled.div`
         background-color:#e2e2e2;
-        width: 100%;
-        height: 300px;
+        display: block;
+        height: 500px;
         border: 1px solid red;
         margin: 25px;
         text-align: center;
-
         h3{
             color: #d30226;
             font-size:  40px;
@@ -38,14 +38,29 @@ const InfoIcon = styled.div`
         color: red;
 
         .icon-style{
-            width: 50px;
-            height: 20px;
-            
+            font-size: 60px;
         }
 
         p{
             font-size: 25px;
         }
+`;
+
+const DatoEmpresa = styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: red;
+        color: white;
+        width: 100%;
+        height: 150px;
+        padding: 10px;
+        p{
+            font-size: 15px;
+            padding: 10px;
+            margin: 10px;
+        }
+
 `;
 
 const VisionMision = styled.div`
@@ -57,7 +72,21 @@ const VisionMision = styled.div`
 `;
 
 const InfoVM = styled.div`
-    
+        text-align: center;
+        justify-content: center;
+        color: red;
+        padding: 10px;
+
+        .icon-style{
+            font-size: 50px;
+
+        }
+
+        p{
+            font-size: 15px;
+            padding: 10px;
+            margin: 10px;
+        }
 `;
 
 export function InfoEmpresa(){
@@ -79,9 +108,18 @@ export function InfoEmpresa(){
             </InfoIcon>
         </IconContainer>
         
+        <DatoEmpresa>
+            <p>Ser la mejor empresa en la creación de imagen, utilizando materiales de calidad para de esta manera lograr la satisfacci&oacute;n de nuestros clientes.</p>
+        </DatoEmpresa>
+        
         <VisionMision>
             <InfoVM>
-            <p>Ser la mejor empresa en la creación de imagen, utilizando materiales de calidad para de esta manera lograr la satisfacci&oacute;n de nuestros clientes.</p>
+                < GiArrowScope className="icon-style"/>
+                <p>Lograr un crecimiento y una mayor participaci&oacute;n en el mercado de la impresi&oacute;n, dejando huella en cada uno de nuestros clientes.</p>
+            </InfoVM>
+            <InfoVM>
+                < IoEyeOutline className="icon-style"/>
+                <p>Ser la mejor empresa en la creaci&oacute;n de imagen, utilizando materiales de calidad para de esta manera lograr la satisfacci&oacute;n de nuestros clientes.</p>
             </InfoVM>
         </VisionMision>
         
